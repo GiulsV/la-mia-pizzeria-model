cf79556e635262f144393cecd9e29e718e6ef244
GiulsV <valente.giulia93@gmail.com>
2022-11-21 19:13:32 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,27 @@
+﻿using la_mia_pizzeria_model.Models;
+using la_mia_pizzeria_model.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_model.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            PizzeriaDbContext db = new PizzeriaDbContext();
+
+            List<Pizza> listaPizze = db.Pizze.ToList();
+            return View(listaPizze);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            PizzeriaDbContext db = new PizzeriaDbContext();
+
+            Pizza pizza = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+            return View(pizza);
+        }
+    }
+}
